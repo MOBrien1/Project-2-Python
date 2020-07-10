79176e72b5d954c0ce0c4cb9d52686be240b0942
MOBrien1 <67173712+MOBrien1@users.noreply.github.com>
2020-07-10 20:14:55 +0800

    move file into repository

---
+++ students/part1/tests/test_1_code.py
@@ -0,0 +1,49 @@
+def process_weather(forecast_file):
+    """Converts raw weather data into meaningful text.
+
+    Args:
+        forecast_file: A string representing the file path to a file
+            containing raw weather data.
+    Returns:
+        A string containing the processed and formatted weather data."""
+    
+#5 Day Overview
+#The lowest temperature will be 8.3°C, and will occur on Friday 19 June 2020.
+#The highest temperature will be 22.2°C, and will occur on Sunday 21 June 2020.
+#The average low this week is 11.7°C.
+#The average high this week is 20.1°C.
+
+
+#-------- Date --------
+#Minimum Temperature:
+#Maximum Temperature:
+#Daytime: 
+    #Chance of rain:  
+#Nighttime: 
+    #Chance of rain:  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    with open(forecast_file) as f:
+        forecast_file = data/
+        weather_data = json.load(f)
+        print(weather_data)
+
